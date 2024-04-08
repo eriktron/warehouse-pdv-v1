d2d69c6f90f6f5baa0f6570321d09e311d228ad9
btron <contreras.eee@gmail.com>
2024-04-08 08:00:57 -0400

    aun trabajando en migracion uno a uno, no complita error en los nombre

---
+++ services/customer.services.js
@@ -0,0 +1,39 @@
+const boom = require('@hapi/boom');
+
+//const getConnection = require('../libs/postgres');
+const { models } = require ('../libs/sequelize');
+
+class CustomerService {
+  constructor() {}
+
+  async find() {
+    const rta = await models.Customer.findAll();
+    return rta;
+  }
+
+  async findOne(id) {
+    const user = await models.Customer.findByPk(id);
+    if(!user){
+      throw boom.notFound('Customer-Cliente no encontrado');
+    }
+    return user;
+  }
+  async create(data){
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async update(id, changes) {
+    const model = await this.findOne(id);
+    const rta = await model.update(changes);
+    return rta;
+  }
+
+  async delete(id) {
+    const model = await this.findOne(id);
+    await model.destroy();
+    return { rta: true };
+  }
+}
+
+module.exports = CustomerService;
